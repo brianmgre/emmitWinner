d8cf8ec6d05b09d56f746e6d0de93900f3360866
Brian G <brian.gregorius@gmail.com>
2023-03-26 16:42:09 -0600

    updated project

---
+++ contracts/MakeAttempt.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+interface WinnerContract {
+    function attempt() external;
+}
+
+contract MakeAttempt {
+    function sendAttempt(address _contract) public {
+        return WinnerContract(_contract).attempt();
+    }
+}
